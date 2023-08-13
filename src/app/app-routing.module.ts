70fd4de0e673dfe5fbf231d5b35b1d03ecb1145f
Bavly <137098046+Bavly-Kamel@users.noreply.github.com>
2023-08-13 17:20:51 +0300

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'product/:id',
+    loadChildren: () =>
+      import('./product-details/product-details.module').then(
+        (exports) => exports.ProductDetailsModule
+      ),
+    title: 'Product',
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/products.module').then(
+        (exports) => exports.ProductsModule
+      ),
+    title: 'Products',
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes),
+    // { PreloadingStrategy: PreloadAllModules },
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
